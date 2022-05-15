22f9f744a332b9801cf7df860fc243b375ddc931
Jerom Iruthayaraj <jeromraj99@gmail.com>
2022-05-15 14:35:47 +0530

    add DC engine and event handlers

--- src/main/webapp/Components/main.js
+++ src/main/webapp/Components/main.js
@@ -5,6 +5,54 @@ $(document).ready(function () {
   $("#alertError").hide();
 });
 
+// SAVE ============================================
+$(document).on("click", "#btnSave", function (event) {
+  // Clear alerts---------------------
+  $("#alertSuccess").text("");
+  $("#alertSuccess").hide();
+  $("#alertError").text("");
+  $("#alertError").hide();
+  // Form validation-------------------
+  var status = validateComplaintForm();
+  if (status != true) {
+    $("#alertError").text(status);
+    $("#alertError").show();
+    return;
+  }
+  // If valid------------------------
+  var type = $("#hidComplaintIDSave").val() == "" ? "POST" : "PUT";
+  $.ajax({
+    url: "ComplaintsAPI",
+    type: type,
+    data: $("#formComplaint").serialize(),
+    dataType: "text",
+    complete: function (response, status) {
+      onComplaintSaveComplete(response.responseText, status);
+    },
+  });
+});
+
+// UPDATE==========================================
+$(document).on("click", ".btnUpdate", function (event) {
+  $("#hidComplaintIDSave").val($(this).data("complaintid"));
+  $("#customerName").val($(this).closest("tr").find("td:eq(0)").text());
+  $("#phoneNumber").val($(this).closest("tr").find("td:eq(1)").text());
+  $("#accountNumber").val($(this).closest("tr").find("td:eq(2)").text());
+  $("#message").val($(this).closest("tr").find("td:eq(3)").text());
+});
+
+$(document).on("click", ".btnRemove", function (event) {
+  $.ajax({
+    url: "ComplaintsAPI",
+    type: "DELETE",
+    data: "complaintID=" + $(this).data("complaintid"),
+    dataType: "text",
+    complete: function (response, status) {
+      onComplaintDeleteComplete(response.responseText, status);
+    },
+  });
+});
+
 // CLIENT-MODEL================================================================
 function validateComplaintForm() {
   // CODE
@@ -30,3 +78,47 @@ function validateComplaintForm() {
   }
   return true;
 }
+
+function onComplaintSaveComplete(response, status) {
+  if (status == "success") {
+    var resultSet = JSON.parse(response);
+    if (resultSet.status.trim() == "success") {
+      $("#alertSuccess").text("Successfully saved.");
+      $("#alertSuccess").show();
+      $("#divComplaintsGrid").html(resultSet.data);
+    } else if (resultSet.status.trim() == "error") {
+      $("#alertError").text(resultSet.data);
+      $("#alertError").show();
+    }
+  } else if (status == "error") {
+    $("#alertError").text("Error while saving.");
+    $("#alertError").show();
+  } else {
+    $("#alertError").text("Unknown error while saving..");
+    $("#alertError").show();
+  }
+
+  $("#hidComplaintIDSave").val("");
+  $("#formComplaint")[0].reset();
+}
+
+function onComplaintDeleteComplete(response, status) {
+  if (status == "success") {
+    var resultSet = JSON.parse(response);
+    if (resultSet.status.trim() == "success") {
+      $("#alertSuccess").text("Successfully deleted.");
+      $("#alertSuccess").show();
+      $("#divComplaintsGrid").html(resultSet.data);
+    } else if (resultSet.status.trim() == "error") {
+      $("#alertError").text(resultSet.data);
+      $("#alertError").show();
+    }
+  } else if (status == "error") {
+    $("#alertError").text("Error while deleting.");
+    $("#alertError").show();
+  } else {
+    $("#alertError").text("Unknown error while deleting..");
+    $("#alertError").show();
+  }
+}
+  
