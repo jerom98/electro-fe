fc18b4c3f657679532792d5ad894882d5fbb0510
Jerom Iruthayaraj <jeromraj99@gmail.com>
2022-05-15 14:52:54 +0530

    add api servlet

--- src/main/java/com/ComplaintsAPI.java
+++ src/main/java/com/ComplaintsAPI.java
@@ -6,49 +6,92 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Servlet implementation class ComplaintsAPI
  */
+@WebServlet("/ComplaintsAPI") 
 public class ComplaintsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ComplaintsAPI() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ComplaintsAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1].replace('+', ' '));
+			}
+		} catch (Exception e) {
+		}
+		return map;
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Complaint complaintObj = new Complaint();
+		String output = complaintObj.addComplaint(request.getParameter("customerName"), request.getParameter("phoneNumber"),
+				request.getParameter("accountNumber"), request.getParameter("message"));
+		response.getWriter().write(output);
 		doGet(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Complaint complaintObj = new Complaint();
+		Map paras = getParasMap(request);
+		String output = complaintObj.updateComplaint(paras.get("hidComplaintIDSave").toString(), paras.get("customerName").toString(),
+				paras.get("phoneNumber").toString(), paras.get("accountNumber").toString(), paras.get("message").toString());
+		response.getWriter().write(output);
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Complaint complaintObj = new Complaint();
+		Map paras = getParasMap(request);
+		String output = complaintObj.deleteComplaint(paras.get("complaintID").toString());
+		response.getWriter().write(output);
 	}
 
 }
+
